3286c3ac1c6c2a025082425dd2b472e3e3d642bc
João <joao_gabriel@live.com.pt>
2022-05-25 17:57:44 -0300

    rota para usuario logar e obter token de autenticação

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,3 +1,40 @@
 const connection = require("../services/database/connection");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(404)
+      .json("É necessário informar email e senha para acessar");
+  }
+
+  try {
+    const userExists = await connection("users").where({ email }).first();
+    if (!userExists) {
+      return res.status(401).json("Email e/ou senha incorretos");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, userExists.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json("Email e/ou senha incorretos");
+    }
+
+    const { password: _, ...userData } = userExists;
+
+    const token = jwt.sign(userData, process.env.JWT_SECRET, {
+      expiresIn: "2hr",
+    });
+
+    return res.status(200).json({ user: userData, token });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+module.exports = {
+  login,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { signUpUser } = require("./controllers/users");
+const { login } = require("./controllers/login");
 
 const router = express();
 
-router.post("/usuarios", signUpUser)
+router.post("/usuarios", signUpUser);
+router.post("/login", login);
 
 module.exports = router;
