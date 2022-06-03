e97e0ab5a01482a235d24622ea6c20f7f2f425b4
João <joao_gabriel@live.com.pt>
2022-06-02 21:25:18 -0300

    fix/validação do yup

--- src/validations/schemaEditClient.js
+++ src/validations/schemaEditClient.js
@@ -3,18 +3,23 @@ const yup = require("yup");
 const schema = yup.object().shape({
   address: yup.string(),
   complement: yup.string(),
-  postal_code: yup
-    .string()
-    .matches(/^(|.{8,8})$/, "O código postal deve ter oito caractéres"),
+  postal_code: yup.string().matches(/^(|.{8,8})$/, {
+    excludeEmptyString: true,
+    matches: "O código postal deve ter oito caractéres",
+  }),
   district: yup.string(),
   city: yup.string(),
-  state: yup
-    .string()
-    .matches(/^(|.{2,2})$/, "O estado deve ter dois caractéres"),
+  state: yup.string().matches(/^(|.{2,2})$/, {
+    excludeEmptyString: true,
+    matches: "O estado deve ter dois caractéres",
+  }),
   cellphone: yup.string().required("O campo celular é obrigatório"),
   cpf: yup
     .string()
-    .matches(/^(|.{11,11})$/, "O CPF deve ter onze caractéres")
+    .matches(/^(|.{11,11})$/, {
+      excludeEmptyString: true,
+      matches: "O CPF deve ter onze caractéres",
+    })
     .required("O campo CPF é obrigatório"),
   email: yup
     .string()
