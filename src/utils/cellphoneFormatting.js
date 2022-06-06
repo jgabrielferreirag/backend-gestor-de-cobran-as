0a5d82127492aa2a94d5b96440bef13d1faa4dd2
João <joao_gabriel@live.com.pt>
2022-06-06 12:15:21 -0300

    fix/ correção na formatação do celular

--- src/utils/cellphoneFormatting.js
+++ src/utils/cellphoneFormatting.js
@@ -1,8 +1,10 @@
 const formatCellphone = (cellphone) => {
+  console.log(cellphone);
   formattedCellphone = `${cellphone.slice(0, 2)} ${cellphone.slice(
     2,
     3
-  )} ${cellphone.slice(3, 7)} ${cellphone.slice(8)}`;
+  )} ${cellphone.slice(3, 7)} ${cellphone.slice(7)}`;
+  console.log(formattedCellphone);
   return formattedCellphone;
 };
 
