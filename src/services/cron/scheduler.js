3efd0e95f765e7accc36f7a735e88d9024b2273b
João <joao_gabriel@live.com.pt>
2022-06-10 15:26:36 -0300

    horario cron

--- src/services/cron/scheduler.js
+++ src/services/cron/scheduler.js
@@ -2,23 +2,28 @@ const cronJob = require("node-cron");
 const connection = require("../database/connection");
 
 const initScheduledJob = () => {
-  const scheduledJob = cronJob.schedule("0 11 * * *", async () => {
-    const today = new Date().setHours(0, 0, 0, 0);
-    const bills = await connection("bills")
-      .where({ status: "Pendente" })
-      .select("id", "due_date");
-    const expiredBills = bills.filter((bill) => {
-      return ++bill.due_date < today;
-    });
+  const scheduledJob = cronJob.schedule(
+    "35 18 * * *",
+    async () => {
+      console.log("entrou no loop");
+      const today = new Date().setHours(0, 0, 0, 0);
+      const bills = await connection("bills")
+        .where({ status: "Pendente" })
+        .select("id", "due_date");
+      const expiredBills = bills.filter((bill) => {
+        return ++bill.due_date < today;
+      });
 
-    const billsToChange = expiredBills.map((bill) => {
-      return bill.id;
-    });
+      const billsToChange = expiredBills.map((bill) => {
+        return bill.id;
+      });
 
-    const updatedBillStatus = await connection("bills")
-      .update({ status: "Vencida" })
-      .whereIn("id", billsToChange);
-  });
+      const updatedBillStatus = await connection("bills")
+        .update({ status: "Vencida" })
+        .whereIn("id", billsToChange);
+    },
+    { timezone: "Africa/Abidjan" }
+  );
   scheduledJob.start();
 };
 
