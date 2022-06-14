b52aa143c3ce0cf1ca740f3ff53b8391fa4e60e0
João <joao_gabriel@live.com.pt>
2022-06-14 12:11:12 -0300

    fix await

--- src/services/scheduler/scheduler.js
+++ src/services/scheduler/scheduler.js
@@ -1,27 +1,25 @@
-
 const connection = require("../database/connection");
 
-const initScheduledJob = () => {
-    const today = new Date().setHours(0, 0, 0, 0);
-    const bills = await connection("bills")
-      .where({ status: "Pendente" })
-      .select("id", "due_date");
-    const expiredBills = bills.filter((bill) => {
-      return ++bill.due_date < today;
-    });
+const initScheduledJob = async () => {
+  const today = new Date().setHours(0, 0, 0, 0);
+  const bills = await connection("bills")
+    .where({ status: "Pendente" })
+    .select("id", "due_date");
+  const expiredBills = bills.filter((bill) => {
+    return ++bill.due_date < today;
+  });
 
-    const billsToChange = expiredBills.map((bill) => {
-      return bill.id;
-    });
+  const billsToChange = expiredBills.map((bill) => {
+    return bill.id;
+  });
 
-    const updatedBillStatus = await connection("bills")
-      .update({ status: "Vencida" })
-      .whereIn("id", billsToChange);
+  const updatedBillStatus = await connection("bills")
+    .update({ status: "Vencida" })
+    .whereIn("id", billsToChange);
 
-    const updateClients = await connection.raw(
-      "update clients set client_status = 'Inadimplente' from bills where clients.id = bills.client_id and bills.status = 'Vencida'"
-     );
-  
+  const updateClients = await connection.raw(
+    "update clients set client_status = 'Inadimplente' from bills where clients.id = bills.client_id and bills.status = 'Vencida'"
+  );
 };
 
-initScheduledJob()
+initScheduledJob();
