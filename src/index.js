b8e6b9dd745ef415154c4e49fe54700d6d3b4f66
João <joao_gabriel@live.com.pt>
2022-06-14 12:08:46 -0300

    teste heroku scheduler

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
-const billScheduledJob = require("./services/scheduler/billScheduler");
-const clientScheduledJob = require("./services/scheduler/clientScheduler");
 
 const app = express();
-
-billScheduledJob();
-clientScheduledJob();
 
 app.use(express.json());
 
